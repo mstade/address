342649e0e42583c9fac6134be637b1e3cb4b947a
Aaron Haines <aaron.haines@rbs.com>
2014-02-14 16:50:01 +0000

    Removed plugin logic

--- web.js
+++ web.js
@@ -3,26 +3,7 @@ define(
   , 'text!./resource.json'
   ]
 , function(parser, resources) {
-
-    return {
-
-      load: function (name, req, onload, config) {
-
-        if (config.isBuild) {
-          onload()
-          return
-        }
-
-        if(err) {
-          onload(err)
-          return
-        }
-
-        var web = parser(resources)
-        onload(web)
-      }
-
-    }
+    return parser(resources)
   }
 )
   
