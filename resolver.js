42aaaad5dad151a84a13acf7ace1d2998ff109a7
Aaron Haines <aaron.haines@rbs.com>
2014-03-20 17:03:56 +0000

    Support multiple methods in a module

--- resolver.js
+++ resolver.js
@@ -6,6 +6,7 @@ define(
       return function(req, res) {
         var scope = this
         require([dep], function(fn) {
+          fn = fn[req.method] || fn
           fn.call(scope, req, res)
         })
       }
