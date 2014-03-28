429f15574ccea14d49d91dbc376948d1aa8b708d
Aaron Haines <aaron.haines@rbs.com>
2014-03-28 18:23:48 +0000

    Log error if module returns non-function

--- resolver.js
+++ resolver.js
@@ -2,11 +2,21 @@ define(
   []
   , function() {
 
+    function isFn(inst){
+      return typeof inst === "function"
+    }
+
     return function (dep) {
       return function(req, res) {
         var scope = this
         require([dep], function(fn) {
           fn = fn[req.method] || fn
+
+          if(!isFn(fn)) {
+            console.debug("failed to resolve resource function from module", dep, req.method)
+            return
+          }
+
           fn.call(scope, req, res)
         })
       }
