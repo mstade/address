a289f063a2e7d62e49b912d148c02f6e252738b1
Leon Coto <leon.coto@rbs.com>
2015-07-06 16:09:53 +0100

    Moving function declaration to the top

--- find-closest.js
+++ find-closest.js
@@ -2,6 +2,12 @@ define(function(require) {
 
   var _ = require('underscore')
 
+  function findClosest(fn, node, baseCase) {
+    if (!node) return baseCase
+    if (fn(node)) return node
+    return findClosest(fn, node.parentNode, baseCase)
+  }
+
   findClosest.anchor = function(node, baseCase) {
     return findClosest(function(n) {
       return n.nodeName == 'A' && n.href
@@ -16,11 +22,4 @@ define(function(require) {
   }
 
   return findClosest
-
-  function findClosest(fn, node, baseCase) {
-    if (!node) return baseCase
-    if (fn(node)) return node
-    return findClosest(fn, node.parentNode, baseCase)
-  }
-
 })
