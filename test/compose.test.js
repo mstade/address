ccd18518c89fb708ff6966f025a8687fbdb37ed9
Peter Galiba <Peter.Galiba@rbs.com>
2015-10-09 10:22:13 +0100

    Add test for compose

---
+++ test/compose.test.js
@@ -0,0 +1,83 @@
+define(function(require) {
+  var Squire = require('Squire')
+  , compose
+  , web
+
+  beforeEach(function(done) {
+
+    var injector = new Squire();
+
+    web = {
+        uri : function(uri, params) {
+          var paramsString = ""
+          Object.keys(params).forEach(function(key) {
+            paramsString += "/" + params[key]
+          })
+          return uri.split("/{")[0] + paramsString
+        }
+      , find: function(v) { return web.routes[v] }
+      , routes: {}
+    }
+
+    injector.mock(
+    'web'
+    , function() {
+      return {
+        load: function(name, req, onload, config) {
+          onload(web)
+        }
+      }
+    })
+    .require(
+    [ 'compose' ]
+    , function(a) {
+      compose = a
+      done()
+    }
+    )
+  })
+
+  describe('Compose', function() {
+
+    it('should return the requested resource if no routes are defined', function () {
+      compose('a', 'b').should.equal('a')
+    })
+
+    it('should return the requested resource if route is defined', function () {
+      web.routes.a = {
+        name : 'b'
+      , path : '/a'
+      , methods : {}
+      , composes : []
+      , redirects : {}
+      }
+      web.routes.b = {
+        name : 'b'
+      , path : '/b'
+      , methods : {}
+      , composes : []
+      , redirects : {}
+      }
+      compose('a', 'b').should.equal('a')
+    })
+    it('should return the composed resource if route is composable', function () {
+      web.routes.a = {
+        name : 'b'
+      , path : '/a/{id}'
+      , methods : {}
+      , composes : []
+      , redirects : {}
+      }
+      web.routes.b = {
+        name : 'b'
+      , path : '/b/{id}'
+      , methods : {}
+      , composes : ['/a/{id}']
+      , params: ['{id}']
+      , redirects : {}
+      }
+      compose('a', 'b').should.equal('/b/{id}')
+    })
+  })
+  }
+)
