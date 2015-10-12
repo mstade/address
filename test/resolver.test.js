f59f4e4f7a332541947a3cfb26f823ea52cd9617
Peter Galiba <Peter.Galiba@rbs.com>
2015-10-12 10:49:37 +0100

    Add resolver tests

--- test/resolver.test.js
+++ test/resolver.test.js
@@ -4,37 +4,46 @@ define(
   , 'jquery'
   ]
   , function(Squire, sinon, $) {
-
-    var resolver
-      , cbSpy
-      , requireStub
-
-    beforeEach(function(done) {
-
-      var injector = new Squire()
-
-      injector.require(
-        [ 'resolver' ]
-      , function(r) {
-          resolver = r
-          cbSpy = sinon.spy()
-          requireStub = sinon.stub(window, "require", function(dep, cb) {
-            cb(cbSpy)
-          })
-
-          done()
-        }
-      )
-    })
-
-    afterEach(function() {
-      requireStub.restore()
-    })
-
     describe('Resolver', function() {
 
-      it('should return a resolver configured with a require module name', function() {
+      var resolver
+      , cbSpy
+      , requireStub
+      , logSpy
 
+      beforeEach(function(done) {
+
+        var injector = new Squire()
+
+        injector.mock('logger/log', function() {
+          logSpy = {
+            debug: sinon.spy()
+          }
+          return {
+            load: function(name, req, onload, config) {
+              onload(logSpy)
+            }
+          }
+        })
+        .require(
+          [ 'resolver' ]
+        , function(r) {
+            resolver = r
+            cbSpy = sinon.spy()
+            requireStub = sinon.stub(window, "require", function(dep, cb) {
+              cb(cbSpy)
+            })
+
+            done()
+          }
+        )
+      })
+
+      afterEach(function() {
+        requireStub.restore()
+      })
+
+      it('should return a resolver configured with a require module name', function() {
         var resolve = resolver("require/module")
           , type = typeof resolve
           , req = {}
@@ -50,6 +59,34 @@ define(
 
         cbSpy.should.have.been.calledOnce
         cbSpy.should.have.been.calledWith(req)
+      })
+
+      it('should log when no function returned after resolve', function() {
+        cbSpy = {}
+        var resolve = resolver("require/module")
+          , res = sinon.spy()
+
+        resolve.call(null, {}, res)
+        logSpy.debug.should.have.been.calledOnce
+      })
+
+      it('should return a resolver when called returns a function that calls the response', function() {
+        cbSpy = sinon.spy(function(req, fn) {
+          var fnSpy = sinon.spy(fn)
+            , type = typeof fn
+          type.should.equal('function')
+          fnSpy()
+          fnSpy(null)
+        })
+        var resolve = resolver("require/module")
+          , res = sinon.spy()
+          , req = {}
+
+        resolve.call(null, req, res)
+        cbSpy.should.have.been.calledOnce
+        res.should.have.been.calledTwice
+        res.should.have.been.calledWith()
+        res.should.have.been.calledWith(null)
       })
     })
   }
