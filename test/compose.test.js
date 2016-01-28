94f212cc1378edef9f94f6248a8727ed1db85486
Peter Galiba <Peter.Galiba@rbs.com>
2016-01-28 15:25:07 +0000

    Fix params sent as query string for optional ones

--- test/compose.test.js
+++ test/compose.test.js
@@ -2,6 +2,7 @@ define(function(require) {
   var Squire = require('Squire')
   , compose
   , web
+  , _ = require('underscore')
 
   describe('Compose', function() {
     beforeEach(function(done) {
@@ -10,18 +11,14 @@ define(function(require) {
 
       web = {
         uri : function(uri, params) {
-          var paramsString = ""
-          Object.keys(params).forEach(function(key) {
-            paramsString += "/" + params[key]
-          })
-          return uri.split("/{")[0] + paramsString
+          return _.template(uri, {interpolate: /\{(.+?)\}/g })(params)
         }
         , find: function(v) { return web.routes[v] }
         , routes: {}
       }
 
       injector.mock(
-      'web'
+        'web'
       , function() {
         return {
           load: function(name, req, onload, config) {
@@ -30,7 +27,7 @@ define(function(require) {
         }
       })
       .require(
-      [ 'compose' ]
+        [ 'compose' ]
       , function(a) {
         compose = a
         done()
@@ -44,7 +41,7 @@ define(function(require) {
 
     it('should return the requested resource if route is defined', function () {
       web.routes.a = {
-        name : 'b'
+        name : 'a'
       , path : '/a'
       , methods : {}
       , composes : []
@@ -61,7 +58,7 @@ define(function(require) {
     })
     it('should return the composed resource if route is composable', function () {
       web.routes.a = {
-        name : 'b'
+        name : 'a'
       , path : '/a/{id}'
       , methods : {}
       , composes : []
@@ -76,6 +73,25 @@ define(function(require) {
       , redirects : {}
       }
       compose('a', 'b').should.equal('/b/foo')
+    })
+    it('should return the composed resource if route is composable with optional parameters', function () {
+      web.routes['/news-reader/articles/2'] = {
+        "name": "a"
+      , "path": "/news-reader/articles/{articleId}"
+      , "methods": {}
+      , composes : []
+      , params: {articleId: 2}
+      , redirects : {}
+      }
+      web.routes['/news-reader/1'] = {
+        "name": "b"
+      , "path": "/news-reader(/{articleId})"
+      , "methods": {}
+      , "composes" : [ "/news-reader/articles/{articleId}" ]
+      , params: {articleId: 1}
+      , redirects : {}
+      }
+      compose('/news-reader/articles/2', '/news-reader/1').should.equal('/news-reader(/2)')
     })
   })
   }
