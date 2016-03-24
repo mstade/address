cfcbb086d663348091fa820c93a03c85effd429e
Peter Galiba <Peter.Galiba@rbs.com>
2016-03-24 11:09:02 +0000

    Enabled tests for view-invoker

--- test/view-invoker.test.js
+++ test/view-invoker.test.js
@@ -1,73 +1,44 @@
-define(
-  [ 'Squire'
-  , 'sinon'
-  ]
-  , function(Squire, sinon) {
-    xdescribe('View Invoker', function() {
+define(function(require) {
+  var viewInvoker = require('view-invoker'),
+      sinon = require('sinon')
 
-      var viewInvoker
-        , logSpy
-
-      beforeEach(function(done) {
-
-        var injector = new Squire()
-
-        injector.mock('logger/log', function() {
-          logSpy = {
-            debug: sinon.spy()
+  describe('View Invoker', function() {
+    it('should call the responses body with the request\'s context', function() {
+      var bodySpy = sinon.spy()
+        , req = {
+            context: {}
           }
-          return {
-            load: function(name, req, onload, config) {
-              onload(logSpy)
-            }
-          }
-        })
-        .require(
-          [ 'view-invoker' ]
-        , function(r) {
-            viewInvoker = r
-            done()
-          }
-        )
-      })
+        , res = {
+          statusCode: 200,
+          body: bodySpy
+        }
+      viewInvoker(req, res)
+      bodySpy.should.have.been.calledOnce
+      bodySpy.should.have.been.calledWith(req.context)
+    })
 
-      it('should call the responses body with the request\'s context', function() {
-        var bodySpy = sinon.spy()
-          , req = {
-              context: {}
-            }
-          , res = {
-            statusCode: 200,
+    it('should not call the responses body if response is not 200', function() {
+      var bodySpy = sinon.spy()
+        , req = {
+            context: {}
+          }
+        , res = {
+            statusCode: 404,
             body: bodySpy
           }
-        viewInvoker(req, res)
-        bodySpy.should.have.been.calledOnce
-        bodySpy.should.have.been.calledWith(req.context)
-      })
-
-      it('should log when the status of the response is not 200', function() {
-        var req = {
-              context: {}
-            }
-          , res = {
-              statusCode: 404,
-              body: sinon.spy()
-            }
-        viewInvoker(req, res)
-        logSpy.debug.should.have.been.calledOnce
-      })
-
-      it('should log when the body of the response is not a function', function() {
-        var req = {
-              context: {}
-            }
-          , res = {
-              statusCode: 200,
-              body: 42
-            }
-        viewInvoker(req, res)
-        logSpy.debug.should.have.been.calledOnce
-      })
+      viewInvoker(req, res)
+      bodySpy.should.not.be.called
     })
-  }
-)
+
+    it('should not call the responses body if it is not a function', function() {
+      var req = {
+            context: {}
+          }
+        , res = {
+            statusCode: 200,
+            body: 42
+          }
+      viewInvoker(req, res)
+    })
+  })
+})
