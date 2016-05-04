251ea51fdf57f6e962e2b3e11b9e57941a8a9e44
Peter Galiba <Peter.Galiba@rbs.com>
2016-05-04 14:37:30 +0100

    Cleanup

--- serialize.js
+++ serialize.js
@@ -2,22 +2,19 @@ define(function(require) {
   var _ = require('underscore')
   return function(query, encode) {
     encode = encode || _.identity
-    var sep = '&'
-      , eq = '='
-      , params = Object.keys(query).reduce(function(list, key) {
-          var ks = encode(key) + eq,
-              value = query[key]
+    var params = Object.keys(query).reduce(function(list, key) {
+          var value = query[key]
 
           if (_.isUndefined(value)) return list
-          if (_.isArray(value)) return list.concat(value.map(getEncodedPair))
+          if (_.isArray(value)) return list.concat(value.map(encodeKV))
 
-          return list.concat(getEncodedPair(value))
+          return list.concat(encodeKV(value))
 
-          function getEncodedPair(v) {
-            return ks + encode(v)
+          function encodeKV(v) {
+            return encode(key) + '=' + encode(v)
           }
         }, [])
 
-    return params.length ? '?' + params.join(sep) : ''
+    return params.length ? '?' + params.join('&') : ''
   }
 })
