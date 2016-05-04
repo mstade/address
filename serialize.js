b07e3244f174b1e75bb7be1d365c72f6a9338d29
Peter Galiba <Peter.Galiba@rbs.com>
2016-05-04 11:10:16 +0100

    Added optional encoder to serialize.

--- serialize.js
+++ serialize.js
@@ -1,14 +1,25 @@
-define(
-  []
-, function() {
+define(function(require) {
+  var _ = require('underscore')
+  return function(query, encode) {
+    encode = encode || _.identity
+    var sep = '&'
+      , eq = '='
+      , params = Object.keys(query).reduce(function(list, key) {
+          var ks = encode(key) + eq,
+              value = query[key]
 
-    return function(query) {
-      var params = Object.keys(query).reduce(function(a,k) {
-        a.push(k + '=' + query[k])
-        return a
-      },[])
-      return params.length ? '?' + params.join('&') : ''
-    }
-  
+          if (value == null)
+            return list
+          else if (_.isArray(value))
+            return list.concat(value.map(getEncodedValue).join(sep))
+          else
+            return list.concat(getEncodedValue(value))
+
+          function getEncodedValue(v) {
+            return ks + encode(v)
+          }
+        }, [])
+
+    return params.length ? '?' + params.join(sep) : ''
   }
-)
+})
